1df3f061df86481c3d03f3dffd3ecb06c0b7f2c1
Vitor Gabriel <gabrieloliveirafsociety@gmail.com>
2023-01-12 21:11:30 -0300

    del arquivos build

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ from os import path, environ
 
 class Config():
 	SECRET_KEY = 'd+;2:sD,HY@u]C^1[:ASg}P2[[dCP.;S4*~(>1~c^)6_q`.*xjO]z!pVtD;z#)i1+|/-8'
-	TEMPLATE_FOLDER = path.join(path.dirname(path.abspath(__file__)), 'templates')
 	ROOT_DIR = path.dirname(path.abspath(__file__))
 
 class ConfigDevelopment(Config):
@@ -15,14 +14,14 @@ class ConfigDevelopment(Config):
 class ConfigTesting(Config):
 	DEBUG = True
 	TESTING = True
-	IP_HOST = 'localhost' # Cloud Server
+	IP_HOST = 'encurtador-url-pythonic.up.railway.app' # Cloud Server
 	PORT_HOST = 80
 	SQLALCHEMY_DATABASE_URI = 'mysql://root:OJgMdk2lrbvpgFXde6po@containers-us-west-47.railway.app:5988/railway' # Temporary DB
 	
 class ConfigProduction(Config):
 	DEBUG = False
 	TESTING = False
-	IP_HOST = 'localhost' # Cloud Server
+	IP_HOST = 'encurtador-url-pythonic.up.railway.app' # Cloud Server
 	PORT_HOST = 80
 	SQLALCHEMY_DATABASE_URI = 'mysql://root:OJgMdk2lrbvpgFXde6po@containers-us-west-47.railway.app:5988/railway' # Temporary DB
 	
